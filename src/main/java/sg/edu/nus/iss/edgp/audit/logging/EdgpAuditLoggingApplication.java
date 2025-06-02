82d23ea5f43785a844b6554648660e9a9e10e5e9
Thet-Nandar-Aung <e1327955@u.nus.edu>
2025-06-03 01:51:56 +0800

    Initial commit: backend setup with database config, AWS secret loading,and security starter.

---
+++ src/main/java/sg/edu/nus/iss/edgp/audit/logging/EdgpAuditLoggingApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.edgp.audit.logging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdgpAuditLoggingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdgpAuditLoggingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sg/edu/nus/iss/edgp/audit/log/EdgpAuditLoggingApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.edgp.audit.log;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdgpAuditLoggingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
